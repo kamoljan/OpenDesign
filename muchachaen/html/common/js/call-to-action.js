ea27850eeac3a7cf94def42233536f6f5c574811
yukayunoue <opendesign.yunoue@gmail.com>
2012-06-27 13:35:03 +0900

    Revert "deleted : unneeded data"
    This reverts commit b1748db14bc3b2e21ef9d2ee89104dbd8c99c517.

--- muchachaen/html/common/js/call-to-action.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-         $('.button a').hover(function(){
-		         $(this).stop().animate({'opacity' : '0'}, 500);
-		 }, function(){$(this).stop().animate({'opacity' : '1'}, 500);});
-});
--- muchachaen/html/common/js/year.js
+++
@@ -1,5 +0,0 @@
-function copyright(){
-myDate = new Date();
-myYear = "Copyright &copy; "+myDate.getFullYear()+" Muchachaen. All rights reserved.";
-document.write(myYear);
-}
